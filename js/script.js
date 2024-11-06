36fb2a694ebd04e945f7f5a7d385f52370633525
Joshi Bhavya <160045396+bhavya-joshi0098@users.noreply.github.com>
2024-11-06 09:44:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'Diwali', 'To All' ],
+			strings: [ 'HAPPY', 'Birthday', 'Sahil sir' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
